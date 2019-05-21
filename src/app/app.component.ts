f6681c530c73272aa8d170b801e8e55e6bd30e4a
yinon <yinon.bentarya@gmail.com>
2019-05-22 00:48:37 +0300

    display single task data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Time } from '@angular/common';
 import { Task } from './interfaces/task';
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import * as fromReducers from './store-entities/reducers';
 import { NewTaskAction } from './store-entities/actions/new-task.actions';
@@ -18,7 +18,7 @@ export class AppComponent {
   tasks$: Observable<Task[]>;
 
   newTaskFormGroup = this.formBuilder.group({
-    formControlTitle: [null]
+    formControlTitle: [null, Validators.required]
   });
 
   constructor(
--- src/app/components/single-task/single-task.component.ts
+++ src/app/components/single-task/single-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/interfaces/task';
 
 @Component({
   selector: 'app-single-task',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-task.component.css']
 })
 export class SingleTaskComponent implements OnInit {
-
+  @Input() task: Task;
+  
   constructor() { }
 
   ngOnInit() {
